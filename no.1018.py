60bec71a34db6d56d3f9f8638a0bee26daca2ba6
haehyoun <gracehpark@naver.com>
2022-04-29 15:54:44 +0900

    11 commit

---
+++ no.1018.py
@@ -0,0 +1,71 @@
+"""
+맞았다!!!!!!!!!!!!!!!!!!!!!!! 2시간 정도? 걸림
+Q)
+지민이는 자신의 저택에서 MN개의 단위 정사각형으로 나누어져 있는 M×N 크기의 보드를 찾았다. 
+어떤 정사각형은 검은색으로 칠해져 있고, 나머지는 흰색으로 칠해져 있다. 
+지민이는 이 보드를 잘라서 8×8 크기의 체스판으로 만들려고 한다.
+
+체스판은 검은색과 흰색이 번갈아서 칠해져 있어야 한다. 
+구체적으로, 각 칸이 검은색과 흰색 중 하나로 색칠되어 있고, 변을 공유하는 두 개의 사각형은 다른 색으로 칠해져 있어야 한다. 
+따라서 이 정의를 따르면 체스판을 색칠하는 경우는 두 가지뿐이다. 
+하나는 맨 왼쪽 위 칸이 흰색인 경우, 하나는 검은색인 경우이다.
+
+보드가 체스판처럼 칠해져 있다는 보장이 없어서, 지민이는 8×8 크기의 체스판으로 잘라낸 후에 몇 개의 정사각형을 다시 칠해야겠다고 생각했다. 
+당연히 8*8 크기는 아무데서나 골라도 된다. 
+지민이가 다시 칠해야 하는 정사각형의 최소 개수를 구하는 프로그램을 작성하시오.
+
+I)
+첫째 줄에 N과 M이 주어진다. N과 M은 8보다 크거나 같고, 50보다 작거나 같은 자연수이다. 
+둘째 줄부터 N개의 줄에는 보드의 각 행의 상태가 주어진다. 
+B는 검은색이며, W는 흰색이다.
+
+O)
+첫째 줄에 지민이가 다시 칠해야 하는 정사각형 개수의 최솟값을 출력한다.
+
+"""
+import sys
+N, M = map(int, input().split()) # MxN 크기의 보드
+
+ex_1 = [
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW'
+]
+
+ex_2 = [
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB',
+'BWBWBWBW',
+'WBWBWBWB'
+]
+
+board = []
+for n in range(N):
+    board.append(sys.stdin.readline())
+
+cnt_list = []
+
+# 입력 받은 MxN 보드에서 8x8로 자를 수 있는 모든 경우의 수
+for m in range(M-7): # 행
+    for n in range(N-7): # 열
+        cnt_1 = 0 # 가장 위, 왼쪽이 W일때
+        cnt_2 = 0 # 가장 위, 왼쪽이 B일때
+        for k in range(8):
+            for l in range(8):
+                if board[n+l][m+k] != ex_1[l][k] : # W로 시작할 때
+                    cnt_1 = cnt_1+1
+
+                if board[n+l][m+k] != ex_2[l][k]: # B로 시작할 때
+                    cnt_2 = cnt_2 + 1
+        cnt_list.append(min(cnt_1, cnt_2))      
+
+print(min(cnt_list))  
